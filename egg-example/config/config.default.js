6d2bcd4763c0a410b2e3fe8feb2ecca2c4a562bc
flygit <1224178565@qq.com>
2020-10-21 23:51:41 +0800

    sequelize数据库的配置和迁移、封装api返回类型和全局异常处理

--- egg-example/config/config.default.js
+++ egg-example/config/config.default.js
@@ -16,7 +16,7 @@ module.exports = (appInfo) => {
   config.keys = appInfo.name + "_1603251908901_4149";
 
   // add your middleware config here
-  config.middleware = [];
+  config.middleware = ["errorHandler"];
 
   // add your user config here
   const userConfig = {
@@ -29,12 +29,36 @@ module.exports = (appInfo) => {
       enable: false,
     },
     // 跨域白名单
-    domainWhiteList: ["http://localhost:7001"],
+    domainWhiteList: ["http://localhost:3333"],
   };
   // 允许跨域的方法
   config.cors = {
     origin: "*",
     allowMethods: "GET, PUT, POST, DELETE, PATCH",
+  };
+
+  config.sequelize = {
+    dialect: "mysql",
+    host: "127.0.0.1",
+    username: "root",
+    password: "root",
+    port: 3306,
+    database: "db_egg",
+    // 中国时区
+    timezone: "+08:00",
+    define: {
+      // 取消数据表名复数
+      freezeTableName: true,
+      // 自动写入时间戳 created_at updated_at
+      timestamps: true,
+      // 字段生成软删除时间戳 deleted_at
+      // paranoid: true,
+      createdAt: "created_time",
+      updatedAt: "updated_time",
+      // deletedAt: 'deleted_time',
+      // 所有驼峰命名格式化
+      underscored: true,
+    },
   };
 
   return {
