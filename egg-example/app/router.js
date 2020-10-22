bb9935d8104b107849e60b18dedfb4a07ae1f0d1
flygit <1224178565@qq.com>
2020-10-22 11:20:43 +0800

    数据库表的设计和迁移、数据模型定义、用户注册功能、参数校验、crypto加密

--- egg-example/app/router.js
+++ egg-example/app/router.js
@@ -1,10 +1,12 @@
-"use strict";
+'use strict';
 
 /**
  * @param {Egg.Application} app - egg application
  */
 module.exports = (app) => {
   const { router, controller } = app;
-  router.get("/", controller.home.index);
-  router.get("/list", controller.home.list);
+  router.get('/', controller.home.index);
+  router.get('/list', controller.home.list);
+  // 用户注册
+  router.post('/reg', controller.user.reg);
 };
--- egg-example/config/plugin.js
+++ egg-example/config/plugin.js
@@ -1,14 +1,18 @@
-"use strict";
+'use strict';
 
 /** @type Egg.EggPlugin */
 module.exports = {
-  // {app_root}/config/plugin.js
-  cors: {
-    enable: true,
-    package: "egg-cors",
-  },
-  sequelize: {
-    enable: true,
-    package: "egg-sequelize",
-  },
+    // {app_root}/config/plugin.js
+    cors: {
+        enable: true,
+        package: 'egg-cors',
+    },
+    sequelize: {
+        enable: true,
+        package: 'egg-sequelize',
+    },
+    valparams: {
+        enable: true,
+        package: 'egg-valparams',
+    },
 };
