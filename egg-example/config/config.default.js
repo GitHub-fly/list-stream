1e347a84cd457c04cf7e777164fa44eb8bd7b22d
flygit <1224178565@qq.com>
2020-10-23 21:30:10 +0800

    登录功能和缓存操作的测试

--- egg-example/config/config.default.js
+++ egg-example/config/config.default.js
@@ -75,11 +75,11 @@ module.exports = (appInfo) => {
         client: {
             port: 6379, // Redis port
             host: '127.0.0.1', // Redis host
-            password: '',
-            db: 1,
+            password: 'auth',
+            db: 0,
         },
     }
-    // jwt 
+    // jwt
     config.jwt = {
         secret: 'qhdgw@45ncashdaksh2!#@3nxjdas*_672',
     }
