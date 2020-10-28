926d8c61fc971d40131deb09d980bb84e96da7e1
flygit <1224178565@qq.com>
2020-10-28 23:49:11 +0800

    首页开发

---
+++ components/common/f-list.vue
@@ -0,0 +1,39 @@
+<template>
+	<view class="list-item" @click="$emit('click')">
+		<image :src="item.background" style="width: 340rpx;height: 340rpx;"></image>
+		<view class="rounded-circle px-2 flex align-center" style="position: absolute; left: 15rpx; top: 15rpx; background-color: rgba(0,0,0,0.4);">
+			<text class="iconfont iconbizhongguanli text-warning mr-1"></text>
+			<text class="text-white font">{{item.money}}</text>
+		</view>
+		<view class="rounded-circle px-2 flex align-center" style="position: absolute;right: 15rpx; top: 15rpx; background-color: rgba(0,0,0,0.4);">
+			<text class="font-sm text-white">人气：</text>
+			<text class="text-white font-sm">{{item.pNumber}}</text> 
+		</view>
+		<view class="rounded-circle flex align-center" style="position: absolute;left: 15rpx; bottom: 15rpx;">
+			<text class="text-white font">{{item.title}}</text>
+		</view>
+		<view class="rounded-circle px-2 flex align-center" style="position: absolute; right: 15rpx;bottom: 15rpx; background-color: rgba(0,0,0,0.4);" >
+			<text style="width:20rpx ; height: 20rpx;" class="rounded-circle bg-danger mr-1"></text>
+			<text class="text-white font-sm">已结束</text>
+		</view>
+	</view>
+</template>
+
+<script>
+export default {
+	props: {
+		item: Object
+	}
+};
+</script>
+
+<style>
+.list-item {
+	background-color: #0056B3;
+	width: 340rpx;
+	height: 340rpx;
+	margin: 5px;
+	box-sizing: border-box;
+	position: relative;
+}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,52 +1,84 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+	<view>
+		<!-- 轮播图 -->
+		<swiper
+			:indicator-dots="true"
+			:autoplay="true"
+			:interval="3000"
+			:duration="200"
+			style="width: 750rpx; height: 250rpx;"
+		>
+			<swiper-item>
+				<image src="../../static/banner/1.jpg" style="width: 750rpx;height: 750rpx;"></image>
+			</swiper-item>
+			<swiper-item>
+				<image src="../../static/banner/2.jpg" style="width: 750rpx;height: 750rpx;"></image>
+			</swiper-item>
+		</swiper>
+		<!-- 列表 -->
+		<view class="flex flex-wrap align-center justify-around">
+			<f-list v-for="(item, index) in list" :key="index" :item="item" @click="openLive"></f-list>
 		</view>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
+import fList from '../../components/common/f-list.vue';
+export default {
+	data() {
+		return {
+			list: [
+				{
+					background: 'https://wx4.sinaimg.cn/mw690/006n20AVly1ge98rz9h4bj32003001l6.jpg',
+					money: 124,
+					pNumber: 875,
+					title: '木轮'
+				},
+				{
+					background: 'https://wx4.sinaimg.cn/mw690/006n20AVly1ge98sdw9hgj31bf1z4kix.jpg',
+					money: 134,
+					pNumber: 398,
+					title: '光阴'
+				},
+				{
+					background: 'https://wx2.sinaimg.cn/mw690/006n20AVly1ge98sa8lr3j32003007wm.jpg',
+					money: 860,
+					pNumber: 975,
+					title: 'draw'
+				},
+				{
+					background: 'https://wx1.sinaimg.cn/mw690/00669kenly1gecl5h52h7j30zj1pttz1.jpg',
+					money: 423,
+					pNumber: 984,
+					title: '屋顶'
+				},
+				{
+					background: 'https://wx1.sinaimg.cn/mw690/00669kenly1gecl5vrvr6j32003004qr.jpg',
+					money: 397,
+					pNumber: 127,
+					title: '天体'
+				},
+				{
+					background: 'https://wx3.sinaimg.cn/mw690/00669kenly1gecla0xde7j3200300x6q.jpg',
+					money: 358,
+					pNumber: 279,
+					title: '云~'
+				}
+			]
+		};
+	},
+	onLoad() {},
+	components: {
+		fList
+	},
+	methods: {
+		openLive() {
+			uni.navigateTo({
+				url: '../live/live'
+			});
 		}
 	}
+};
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
-</style>
+<style></style>
