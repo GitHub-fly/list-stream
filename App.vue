7144c6460c51927d977b9fa704722e58908057fd
flygit <1224178565@qq.com>
2020-10-31 21:46:18 +0800

    直播页——镜头翻转

--- App.vue
+++ App.vue
@@ -10,7 +10,7 @@ export default {
 		// 监听底部导航中间凸起按钮
 		uni.onTabBarMidButtonTap(() => {
 			uni.navigateTo({
-				url: 'pages/create-live/create-live'
+				url: '/pages/create-live/create-live'
 			})
 			console.log('点击了中间按钮');
 		});
