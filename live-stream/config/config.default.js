abf255231c603befe00ac1935c7a605633ee0b61
flygit <1224178565@qq.com>
2020-11-01 17:12:00 +0800

    初始化项目所有配置

--- live-stream/config/config.default.js
+++ live-stream/config/config.default.js
@@ -1,3 +1,4 @@
+/* eslint valid-jsdoc: "off" */
 'use strict'
 
 /**
@@ -11,33 +12,78 @@ module.exports = (appInfo) => {
     const config = (exports = {})
 
     // use for cookie sign key, should change to your own and keep security
-    config.keys = appInfo.name + '_1604217576839_2026'
+    config.keys = appInfo.name + '_1604158088886_8645'
 
     // add your middleware config here
-    config.middleware = ['auth', 'errorHandler']
+    config.middleware = ['errorHandler']
 
     // add your user config here
     const userConfig = {
         // myAppName: 'egg',
     }
 
-    // 这些断点的请求需要 token 鉴权
-    config.auth = {
-        match: ['/logout', '/upload', '/getSize', '/file', '/share'],
-    }
-
     config.security = {
         // 关闭 csrf
         csrf: {
-            enable: false,
+            headerName: 'x-csrf-token',
+            ignore: (ctx) => {
+                return ctx.request.url.startsWith('/api')
+            },
         },
         // 跨域白名单
-        domainWhiteList: ['http://localhost:3000'],
+        // domainWhiteList: ['http://localhost:3000'],
     }
     // 允许跨域的方法
     config.cors = {
         origin: '*',
         allowMethods: 'GET, PUT, POST, DELETE, PATCH',
+    }
+
+    config.sequelize = {
+        dialect: 'mysql',
+        host: '127.0.0.1',
+        username: 'root',
+        password: 'root',
+        port: 3306,
+        database: 'db_live_stream',
+        // 中国时区
+        timezone: '+08:00',
+        define: {
+            // 取消数据表名复数
+            freezeTableName: true,
+            // 自动写入时间戳 created_at updated_at
+            timestamps: true,
+            // 字段生成软删除时间戳 deleted_at
+            // paranoid: true,
+            createdAt: 'created_time',
+            updatedAt: 'updated_time',
+            // deletedAt: 'deleted_time',
+            // 所有驼峰命名格式化
+            underscored: true,
+        },
+    }
+
+    config.valparams = {
+        locale: 'zh-cn',
+        throwError: true,
+    }
+
+    config.crypto = {
+        secret: 'qhdgw@45ncashdaksh2!#@3nxjdas*_672',
+    }
+
+    config.jwt = {
+        secret: 'qhdgw@45ncashdaksh2!#@3nxjdas*_672',
+    }
+
+    // redis存储
+    config.redis = {
+        client: {
+            port: 6379, // Redis port
+            host: '127.0.0.1', // Redis host
+            password: '',
+            db: 2,
+        },
     }
 
     return {
--- live-stream/config/plugin.js
+++ live-stream/config/plugin.js
@@ -6,4 +6,20 @@ module.exports = {
         enable: true,
         package: 'egg-cors',
     },
+    sequelize: {
+        enable: true,
+        package: 'egg-sequelize',
+    },
+    valparams: {
+        enable: true,
+        package: 'egg-valparams',
+    },
+    jwt: {
+        enable: true,
+        package: 'egg-jwt',
+    },
+    redis: {
+        enable: true,
+        package: 'egg-redis',
+    },
 }
