b560d9950850fcd33c5483af916dec4867a75cc7
flygit <1224178565@qq.com>
2020-11-02 14:58:32 +0800

    引入请求库和Vuex

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-import Vue from 'vue'
-import App from './App'
+import Vue from 'vue';
+import App from './App';
 
-Vue.config.productionTip = false
+// 引入 封装的请求库并挂载在 Vue 原型上，使用的时候：this.$H
+import $H from './common/request.js';
 
-App.mpType = 'app'
+// 引入 Vuex 并挂载在 Vue 原型上
+import store from './store/index.js';
+Vue.prototype.$store = store;
+
+Vue.config.productionTip = false;
+
+App.mpType = 'app';
 
 const app = new Vue({
+	store,
 	...App
-})
-app.$mount()
+});
+app.$mount();
