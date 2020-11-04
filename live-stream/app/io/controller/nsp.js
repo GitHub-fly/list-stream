eb4990224ef5b2431860295bfcd9718af561a448
flygit <1224178565@qq.com>
2020-11-04 10:12:56 +0800

    socket通讯功能调试

--- live-stream/app/io/controller/nsp.js
+++ live-stream/app/io/controller/nsp.js
@@ -8,6 +8,13 @@ class NspController extends Controller {
         // 前端传过来的参数
         let message = ctx.args[0]
         console.log(message)
+
+        // 当前的 socket 连接
+        const socket = ctx.socket
+        // 获取 socket 的 id
+        const id = socket.id
+        // 向这个 socket 发送消息
+        socket.emit(id, '来自后端的消息')
     }
 }
 module.exports = NspController
