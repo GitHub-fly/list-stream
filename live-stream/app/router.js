2f9dcf3bc306bf0e441234508ff4876d28d99d5a
flygit <1224178565@qq.com>
2020-11-06 22:53:05 +0800

    删除管理员（一）

--- live-stream/app/router.js
+++ live-stream/app/router.js
@@ -33,6 +33,8 @@ module.exports = (app) => {
     router.get('/admin/manager/create', controller.admin.manager.create)
     router.post('/admin/manager', controller.admin.manager.save)
     router.get('/admin/manager', controller.admin.manager.index)
+    //删除管理员
+    router.get('/admin/manager/delete/:id', controller.admin.manager.delete)
 
     io.of('/').route('joinLive', io.controller.nsp.joinLive)
     io.of('/').route('leaveLive', io.controller.nsp.leaveLive)
