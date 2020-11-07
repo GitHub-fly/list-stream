f18ae4cc724b86439461205f70fa0f58717e4d34
flygit <1224178565@qq.com>
2020-11-07 16:37:37 +0800

    删除和编辑管理员

--- live-stream/app/middleware/error_handler.js
+++ live-stream/app/middleware/error_handler.js
@@ -15,7 +15,7 @@ module.exports = (option, app) => {
             // 记录一条错误日志
             app.emit('error', err, ctx)
 
-            const status = err.status || 500
+            let status = err.status || 500
             // 生产环境时 500 错误的详细错误内容不返回给客户端，因为可能包含敏感信息
             let error =
                 status === 500 && app.config.env === 'prod'
@@ -40,7 +40,6 @@ module.exports = (option, app) => {
                     data: error,
                 }
             }
-
             ctx.status = status
         }
     }
