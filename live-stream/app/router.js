336e672d4f9170685e611c1fef10ff3d30d0257c
flygit <1224178565@qq.com>
2020-11-07 19:18:55 +0800

    直播间观看记录、礼物记录、弹幕记录、关闭直播、删除交互

--- live-stream/app/router.js
+++ live-stream/app/router.js
@@ -78,16 +78,18 @@ module.exports = (app) => {
     router.get('/admin/gift/delete/:id', controller.admin.gift.delete)
     //更新礼物数据接口
     router.post('/admin/gift/:id', controller.admin.gift.update)
-    // // 直播间模块（列表）路由
-    // router.get('/admin/live', controller.admin.live.index)
-    // // 查看指定直播间路由
-    // router.get('/admin/live/look/:id', controller.admin.live.look)
-    // // 查看直播间礼物路由
-    // router.get('/admin/live/gift/:id', controller.admin.live.gift)
-    // // 查看直播间弹幕路由
-    // router.get('/admin/live/comment/:id', controller.admin.live.comment)
-    // // 查看已结束的直播间
-    // router.get('/admin/live/close/:id', controller.admin.live.closelive)
+    // 直播间模块（列表）路由
+    router.get('/admin/live', controller.admin.live.index)
+    // 查看指定直播间路由
+    router.get('/admin/live/look/:id', controller.admin.live.look)
+    // 查看直播间礼物路由
+    router.get('/admin/live/gift/:id', controller.admin.live.gift)
+    // 查看直播间弹幕路由
+    router.get('/admin/live/comment/:id', controller.admin.live.comment)
+    // 查看已结束的直播间
+    router.get('/admin/live/close/:id', controller.admin.live.closelive)
+    // 删除直播间
+    router.get('/admin/live/delete/:id', controller.admin.live.delete)
 
     io.of('/').route('joinLive', io.controller.nsp.joinLive)
     io.of('/').route('leaveLive', io.controller.nsp.leaveLive)
